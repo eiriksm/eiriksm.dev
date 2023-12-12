fcf7a6156990ff293c151c6042d348e5f60eb8e8
Eirik S. Morland <eirik@morland.no>
2023-12-12 12:08:19 +0100

    syntax error

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -6,7 +6,7 @@ wget $DISQUS_XML_URL -O disqus.xml
 echo "unsafe-perm = true" > .npmrc
 npm i
 npm run build
-if [ "$DEPLOY" == "0" ]
+if [ "$DEPLOY" == "0" ]; then
     echo "No deploy since DEPLOY is not set to true"
 else
     npm run deploy
