de883e160ce4b9bdffd39247983a2403322f0633
Eirik Stanghelle Morland <eirik@morland.no>
2025-11-29 19:34:40 +0100

    Update drupal.ts

--- src/lib/drupal.ts
+++ src/lib/drupal.ts
@@ -4,8 +4,8 @@ export const drupal = new DrupalClient(
   process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || "",
   {
     auth: {
-      clientId: process.env.DRUPAL_CLIENT_ID || "",
-      clientSecret: process.env.DRUPAL_CLIENT_SECRET || "",
+      username: process.env.BASIC_AUTH_USERNAME,
+      password: process.env.BASIC_AUTH_PASSWORD,
     },
     previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
   }
