0cf770dd72290e23c4178aed5d1e8408a733413e
Eirik Stanghelle Morland <eirik@morland.no>
2025-11-30 18:05:16 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app"
 import Head from "next/head"
-import "@/app/globals.css"
+import "@/styles/globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import Analytics from "@/components/Analytics"
