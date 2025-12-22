b93376ed489d2d60f1eb330b34d79fea3eaebfc4
Eirik S. Morland <eirik@morland.no>
2025-12-22 07:43:36 +0100

    Fix tests locally

--- tests/src/Context/FeatureContext.php
+++ tests/src/Context/FeatureContext.php
@@ -128,6 +128,18 @@ class FeatureContext extends RawDrupalContext {
    */
   public function iFindTheIssueIDForTheArticle() {
     $element = $this->getSession()->getPage()->find('css', '.comment-link-wrapper a');
+    $waited = 0;
+    while (!$element) {
+        $element = $this->getSession()->getPage()->find('css', '.comment-link-wrapper a');
+        if ($element) {
+            break;
+        }
+        $waited++;
+        if ($waited > 10) {
+            throw new \Exception('Could not find the issue link in the article within 10 seconds');
+        }
+        sleep(1);
+    }
     $href = $element->getAttribute('href');
     $this->lastIssueId = str_replace('https://github.com/eiriksm/eiriksm.dev-comments/issues/', '', $href);
   }
@@ -139,11 +151,6 @@ class FeatureContext extends RawDrupalContext {
     $dir = __DIR__ . '/../../../public/ci_issues/' . $this->lastIssueId;
     @mkdir($dir);
     file_put_contents($dir . '/comments', '[]');
-    $this->cacheBust();
-  }
-
-  protected function cacheBust() {
-
   }
 
   /**
@@ -173,7 +180,6 @@ class FeatureContext extends RawDrupalContext {
   }
 ]
 ');
-    $this->cacheBust();
   }
 
 }
