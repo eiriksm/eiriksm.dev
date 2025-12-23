bf793325e855a32610d3d4286029366fe6b72a74
Eirik Stanghelle Morland <eirik@morland.no>
2025-12-23 07:21:50 +0100

    Honor ISSUE_BASE when building GitHub issue/comment URLs (#192)

--- src/pages/blog/[page].tsx
+++ src/pages/blog/[page].tsx
@@ -7,6 +7,7 @@ import Pagination from "@/components/Pagination"
 import { DrupalJsonApiParams } from "drupal-jsonapi-params"
 import { getDisqusCommentCount } from "@/lib/disqus"
 import { getNodePath } from "@/lib/utils"
+import { getIssueUrl } from "@/lib/github"
 
 const POSTS_PER_PAGE = 10
 
@@ -24,7 +25,7 @@ interface BlogPageProps {
 async function fetchCommentCount(issueId: string, repo: string): Promise<number> {
   try {
     const response = await fetch(
-      `https://api.github.com/repos/${repo}/issues/${issueId}`,
+      getIssueUrl(issueId, repo),
       {
         headers: process.env.GITHUB_TOKEN
           ? { Authorization: `token ${process.env.GITHUB_TOKEN}` }
